c483bd36dc335d05e806d62b9d76e4ed4c0fb74e
hrisimov <140465371+hrisimov@users.noreply.github.com>
2026-03-23 23:29:23 +0200

    Updated `SignInForm` component with `useSignInMutation`

--- src/components/SignInForm.jsx
+++ src/components/SignInForm.jsx
@@ -2,7 +2,6 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
 
-import api from '@/api';
 import { useAuth } from '@/components/AuthProvider';
 import Form from '@/components/Form';
 import TextInput from '@/components/TextInput';
@@ -14,6 +13,7 @@ import {
   Input,
   Separator,
 } from '@/components/ui';
+import useSignInMutation from '@/hooks/mutations/useSignInMutation';
 
 const signInFormSchema = z.object({
   email: z.string().email(),
@@ -27,9 +27,11 @@ const SignInForm = () => {
     resolver: zodResolver(signInFormSchema),
   });
 
+  const signInMutation = useSignInMutation();
+
   const onSubmit = async (data) => {
     try {
-      const response = await api.post('/api/signin', data);
+      const response = await signInMutation.mutateAsync(data);
       setToken(response.data.accessToken);
     } catch (e) {
       form.setError('root', {
@@ -57,10 +59,10 @@ const SignInForm = () => {
           <TextInput control={form.control} name='password' type='password' />
 
           <Button
-            disabled={form.formState.isSubmitting}
+            disabled={signInMutation.isPending}
             onClick={form.handleSubmit(onSubmit)}
           >
-            {form.formState.isSubmitting ? 'Loading...' : 'Sign In'}
+            {signInMutation.isPending ? 'Loading...' : 'Sign In'}
           </Button>
         </Form>
       </CardContent>
