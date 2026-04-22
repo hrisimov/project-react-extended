700a90f84e03eb7b404ea08e18c5ff6b71d2cff1
hrisimov <140465371+hrisimov@users.noreply.github.com>
2026-04-22 20:29:27 +0300

    Added `CreateListingPage` component to `Router`

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import Route from '@/components/Route';
+import CreateListingPage from '@/pages/CreateListingPage';
 import HomePage from '@/pages/HomePage';
 import ListingDetailsPage from '@/pages/ListingDetailsPage';
 import ListingFavoritesPage from '@/pages/ListingFavoritesPage';
@@ -29,6 +30,14 @@ const router = createBrowserRouter([
         element: (
           <Route isProtected>
             <HomePage />
+          </Route>
+        ),
+      },
+      {
+        path: '/listings/create',
+        element: (
+          <Route isProtected>
+            <CreateListingPage />
           </Route>
         ),
       },
