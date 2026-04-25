71c3691bd34f705a550d0cf96a714de7a2143acf
hrisimov <140465371+hrisimov@users.noreply.github.com>
2026-04-25 03:14:42 +0300

    Created `StepperInput` component

---
+++ src/components/StepperInput.jsx
@@ -0,0 +1,25 @@
+import { useController } from 'react-hook-form';
+
+import { Stepper } from '@/components/ui';
+
+const StepperInput = ({ control, name, label, ...rest }) => {
+  const form = useController({ control, name });
+
+  return (
+    <div className='flex flex-col gap-2'>
+      <Stepper
+        label={label}
+        value={form.field.value || 0}
+        onChange={form.field.onChange}
+        {...rest}
+      />
+      {form.fieldState.error?.message && (
+        <div className='text-sm text-red-500'>
+          {form.fieldState.error.message}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default StepperInput;
