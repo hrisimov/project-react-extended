feacb06252f8e984db78375ca7a15c9af81acf00
hrisimov <140465371+hrisimov@users.noreply.github.com>
2026-05-11 02:39:39 +0300

    Added fetching and displaying of `reviews` to `ListingDetailsPage` component

--- src/pages/ListingDetailsPage.jsx
+++ src/pages/ListingDetailsPage.jsx
@@ -2,7 +2,9 @@ import { useParams } from 'react-router-dom';
 
 import DataRenderer from '@/components/DataRenderer';
 import ListingDetailsCard from '@/components/ListingDetailsCard';
+import ReviewList from '@/components/ReviewList';
 import useListingDetailsQuery from '@/hooks/queries/useListingDetailsQuery';
+import useListingReviewsQuery from '@/hooks/queries/useListingReviewsQuery';
 
 const ListingDetailsPage = () => {
   const { listingId } = useParams();
@@ -13,11 +15,27 @@ const ListingDetailsPage = () => {
     isLoading,
   } = useListingDetailsQuery(listingId);
 
+  const {
+    data: { data: reviews } = {},
+    isError: isReviewsError,
+    isLoading: isReviewsLoading,
+  } = useListingReviewsQuery(listingId);
+
   return (
     <div className='container py-4'>
-      <DataRenderer error={isError} isLoading={isLoading}>
-        <ListingDetailsCard listing={listing} />
-      </DataRenderer>
+      <div className='mb-8'>
+        <DataRenderer error={isError} isLoading={isLoading}>
+          <ListingDetailsCard listing={listing} />
+        </DataRenderer>
+      </div>
+      {listing && (
+        <div>
+          <h2 className='mb-4'>Reviews</h2>
+          <DataRenderer error={isReviewsError} isLoading={isReviewsLoading}>
+            <ReviewList reviews={reviews} />
+          </DataRenderer>
+        </div>
+      )}
     </div>
   );
 };
