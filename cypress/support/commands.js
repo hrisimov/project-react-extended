b737eb3118d2202bc5a7144db3e3eee820fde50e
hrisimov <140465371+hrisimov@users.noreply.github.com>
2026-05-14 01:08:19 +0300

    Created `login` custom command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -10,7 +10,15 @@
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
+Cypress.Commands.add('login', (email, password) => {
+    cy.visit('http://localhost:5173/signin');
+
+    cy.get('input[name="email"]').type(email);
+    cy.get('input[name="password"]').type(password);
+    cy.get('button').contains('Sign In').click();
+
+    cy.url().should('not.include', '/signin');
+});
 //
 //
 // -- This is a child command --
