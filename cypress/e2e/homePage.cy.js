6773b3136bef3656005385071efdf1e1d1f728ae
hrisimov <140465371+hrisimov@users.noreply.github.com>
2026-05-14 02:59:12 +0300

    Added a test to check if the `home page` displays the correct number of initial listings

--- cypress/e2e/homePage.cy.js
+++ cypress/e2e/homePage.cy.js
@@ -13,4 +13,13 @@ describe('Home Page', () => {
     cy.get('[data-testid="listing-filters"]').should('exist');
     cy.get('[data-testid="listing-list"]').should('exist');
   });
+
+  it('displays the correct number of initial listings', () => {
+    const expectedListingsCount = 12;
+
+    cy.get('[data-testid="listing-list"] > *').should(
+      'have.length',
+      expectedListingsCount,
+    );
+  });
 });
